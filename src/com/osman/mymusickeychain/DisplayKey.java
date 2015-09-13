e5e26ed209b255db01c28c690bc28edf48b28647
Orschmann <jonjosman@gmail.com>
2015-09-13 16:39:48 -0700

    Keychain
    A tool to easily find the notes and triads in a selected musical key.

--- src/com/osman/mymusickeychain/DisplayKey.java
+++ src/com/osman/mymusickeychain/DisplayKey.java
@@ -13,7 +13,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -72,17 +71,12 @@ public class DisplayKey extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
+			Intent intent = new Intent (this, MainKeychainActivity.class);
+			startActivity(intent);
 			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		default:
+			return super.onOptionsItemSelected(item);
+		}		
 	}	
 	
 	/** Sets the activity title using the MusicKeys Mode and Tonic fields. */
